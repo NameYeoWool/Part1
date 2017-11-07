e378b54208c133a43064acb743849f3272a2cb53
YeoWool <gkaudrl542@daum.net>
2017-11-07 17:32:46 +0900

    button setOnClickListener

--- Ch2/Part1/app/src/main/java/desipline/myapplication/MainActivity.java
+++ Ch2/Part1/app/src/main/java/desipline/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package desipline.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class
 MainActivity extends AppCompatActivity {
@@ -10,5 +13,30 @@ MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼이 클릭되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"두 번째 버튼이 눌렸어요.",Toast.LENGTH_LONG).show();
+                setContentView(R.layout.table_layout);
+            }
+        });
+
+
     }
 }
